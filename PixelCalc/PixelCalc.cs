d68b9f73faa183d754d914318d5b2677cf822cd8
Nithish <nithish.jagadeesan@gmail.com>
2023-06-23 12:25:27 -0400

    Initial commit with solution files

---
+++ PixelCalc/PixelCalc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelCalc
+{
+    public static class PixelCalc
+    {
+        public static double DiagonalInPixelCalculation(double screenWidth, double screenHeight, double diagonalInInches)
+        {
+            //performs diagonal in pixels, PPI, dot pitch calculation  
+            double diagonalInPixels = Math.Sqrt((screenWidth * screenWidth) + (screenHeight * screenHeight));
+            double PPI = diagonalInPixels / diagonalInInches;
+            double dotPitch = (diagonalInInches / diagonalInPixels) * 25.4;
+
+            //returns calculated value
+            return Math.Round(diagonalInPixels);
+        }
+
+        public static double PPICalculation(double screenWidth, double screenHeight, double diagonalInInches)
+        {
+            //performs diagonal in pixels, PPI, dot pitch calculation  
+            double diagonalInPixels = Math.Sqrt((screenWidth * screenWidth) + (screenHeight * screenHeight));
+            double PPI = diagonalInPixels / diagonalInInches;
+            double dotPitch = (diagonalInInches / diagonalInPixels) * 25.4;
+
+            //returns calculated value
+            return Math.Round(PPI, 2);
+        }
+
+        public static double DotPitchCalculation(double screenWidth, double screenHeight, double diagonalInInches)
+        {
+            //performs diagonal in pixels, PPI, dot pitch calculation  
+            double diagonalInPixels = Math.Sqrt((screenWidth * screenWidth) + (screenHeight * screenHeight));
+            double PPI = diagonalInPixels / diagonalInInches;
+            double dotPitch = (diagonalInInches / diagonalInPixels) * 25.4;
+
+            //returns calculated value
+            return Math.Round(dotPitch, 4);
+        }
+    }
+}
--- PixelCalc/Program.cs
+++ PixelCalc/Program.cs
@@ -6,10 +6,92 @@ using System.Threading.Tasks;
 
 namespace PixelCalc
 {
-    internal class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
+            //declaring necessary variables required for the application
+            double screenWidth = 0, screenHeight = 0, diagonalInInches = 0, diagonalInPixels = 0, PPI = 0, dotPitch = 0;
+            bool mainMenu = true;
+
+            //clearing the console
+            Console.Clear();
+
+            //displaying menu options
+            while (mainMenu)
+            {
+                Console.WriteLine("********************************************************************");
+                Console.WriteLine("1. Enter Screen dimensions");
+                Console.WriteLine("2. Exit");
+                Console.WriteLine("********************************************************************");
+                string mainMenuOption = Console.ReadLine();
+
+                //receives screen width, screen height, diagonal input from user if menu option 1 is selected
+                if (mainMenuOption == "1")
+                {
+                    //receives screen width from user and validates if it is double or not
+                    bool enterScreenWidth = true;
+                    while (enterScreenWidth)
+                    {
+                        Console.WriteLine("Enter Screen Width (in pixels):");
+                        String screenWidthString = Console.ReadLine();
+                        if (Double.TryParse(screenWidthString, out screenWidth))
+                        {
+                            enterScreenWidth = false;
+                        }
+                        else
+                        {
+                            enterScreenWidth = true;
+                        }
+                    }
+
+                    //receives screen height from user and validates if it is double or not
+                    bool enterScreenHeight = true;
+                    while (enterScreenHeight)
+                    {
+                        Console.WriteLine("Enter Screen Height (in pixels):");
+                        String screenHeightString = Console.ReadLine();
+                        if (Double.TryParse(screenHeightString, out screenHeight))
+                        {
+                            enterScreenHeight = false;
+                        }
+                        else
+                        {
+                            enterScreenHeight = true;
+                        }
+                    }
+
+                    //receives diagoanal in inches from user and validates if it is double or not
+                    bool enterDiagonalInInches = true;
+                    while (enterDiagonalInInches)
+                    {
+                        Console.WriteLine("Enter Diagonal Size (in inches):");
+                        String diagonalInInchesString = Console.ReadLine();
+                        if (Double.TryParse(diagonalInInchesString, out diagonalInInches))
+                        {
+                            enterDiagonalInInches = false;
+                        }
+                        else
+                        {
+                            enterDiagonalInInches = true;
+                        }
+                    }
+
+                    Console.WriteLine(PixelCalc.DiagonalInPixelCalculation(screenWidth, screenHeight, diagonalInInches));
+                    Console.WriteLine(PixelCalc.PPICalculation(screenWidth, screenHeight, diagonalInInches));
+                    Console.WriteLine(PixelCalc.DotPitchCalculation(screenWidth, screenHeight, diagonalInInches));
+                }
+
+                //exits program if menu option 2 is selected
+                if (mainMenuOption == "2")
+                {
+                    Console.Clear();
+                    Console.WriteLine("********************************");
+                    Console.WriteLine("Thank You!");
+                    Console.WriteLine("********************************");
+                    System.Environment.Exit(1);
+                }
+            }
         }
     }
 }
