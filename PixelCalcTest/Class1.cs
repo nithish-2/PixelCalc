1afa54bc7a7342a9e3c75db0a6037954ee4cfd4b
Nithish <nithish.jagadeesan@gmail.com>
2023-06-23 12:34:11 -0400

    Added test code for valid diagonal calculation with positive inputs

---
+++ PixelCalcTest/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PixelCalc;
+
+namespace PixelCalc
+{
+    public class Class1
+    {
+        [Test]
+        public void DiagonalCalculation_Input20and10and31_OutputValidDiagonal()
+        {
+            // Arrange
+            double screenWidth = 20;
+            double screenHeight = 10;
+            double diagonalInInches = 31;
+
+            // Act
+            double actualDiagonalInPixels = PixelCalc.DiagonalInPixelCalculation(screenWidth, screenHeight, diagonalInInches);
+
+            // Assert
+            Assert.AreEqual(22, actualDiagonalInPixels);
+        }
+    }
+}
